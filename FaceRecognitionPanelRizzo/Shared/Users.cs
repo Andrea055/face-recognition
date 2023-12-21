614ab3781e8a9ea65dfa99b5f5cac8d12941f1a2
andreock <vivanco11.ac@gmail.com>
2023-12-21 13:58:59 +0000

    Revert "blazor panel rizzolo (#21)"
    This reverts commit 7efe70bf5e418040fa54d38aecfead5be5e7d503.

--- FaceRecognitionPanelRizzo/Shared/Users.cs
+++
@@ -1,8 +0,0 @@
-namespace face_recognition.Shared;
-
-public class Users{
-    public static readonly string[] Users_db = new[]
-    {
-        "Lorenzo", "Andrea", "Cellini", "Marco", "Matteo"
-    };
-}
--- FaceRecognitionPanelRizzo/Shared/Variables.cs
+++
@@ -1,6 +0,0 @@
-namespace face_recognition.Shared;
-
-public class Variables{
-    public static string Token { get; set; } = "mytoken";   
-
-}
